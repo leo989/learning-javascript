00db011c00d1173eae4121619725a165a53ec898
leo989 <leo.coaccioli@gmail.com>
2013-03-08 12:55:23 +0100

    corretto

--- 2013-03-08/exercise01.js
+++ 2013-03-08/exercise01.js
@@ -8,7 +8,7 @@ function push_n (n){
 
 var numbers = push_n(12);
 var odd = numbers.filter(function (item){
-	return item % 2 !== 0;
+	return item % 2 === 0;
 })
 
 var dob = odd.map(function (item){
@@ -19,7 +19,6 @@ var div_of_four = dob.filter(function (item){
 	return item % 4 === 0;
 })
 
-div_of_four = [2,3,4];
 var tot = div_of_four.reduce(function (prev, cur){
 	return prev + cur;
 })
